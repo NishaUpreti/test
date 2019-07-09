3d729c474aa91e12483477174cba07fb1a04c297
NishaUpreti <51042153+NishaUpreti@users.noreply.github.com>
2019-07-09 19:01:52 +0530

    Add files via upload

--- login.component.ts
+++ login.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { from } from 'rxjs';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { LoginserviceService } from './loginservice.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  usernameR: any;
+  psdR: any;
   loginform: FormGroup;
-  submitted: boolean;
-  constructor(private _fb: FormBuilder) { }
-
-  ngOnInit() {
+  loginservice: any;
+  Employee: any = [];
+  constructor(private _fb: FormBuilder, private service: LoginserviceService) {
+    this.createForm();
+  }
+  createForm() {
     this.loginform = this._fb.group({
-      username: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(8)]],
-      password: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(8), Validators.pattern('^[a-zA-Z0-9!@#$%^&*()]+&')]]
-    });
+      username: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(4)]],
+      password: ['', [Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]]
+
+    })
+
   }
   get username1() {
     return this.loginform.controls.username;
@@ -23,12 +30,33 @@ export class LoginComponent implements OnInit {
   get password1() {
     return this.loginform.controls.password;
   }
-  onSubmit() {
-    this.submitted = true;
+  submitForm() {
     if (this.loginform.valid) {
-      // alert("Form is valid");
+      alert("form is valid");
     }
-    // else
-    //   alert('Form is invalid');
+    else {
+      alert("form invalid");
+    }
+  }
+  recieve(el) {
+    debugger
+    console.log(el);
+    this.usernameR = el.username1;
+    console.log(this.usernameR);
+    this.psdR = el.psd1;
+    console.log(this.psdR);
+
+  }
+  ngOnInit() {
+    this.showconfig();
+  }
+  showconfig() {
+    // this.loginservice.getconfig().subscribe((data:service)=>this.loginservice={
+    //   this.users=data;
+    // })
+    return this.service.getconfig().subscribe((data: {}) => {
+      this.Employee = data;
+      console.log(this.Employee);
+    })
   }
 }
